2d7c583f30369cfab1d7c0fc871d56bde1a5b74e
Sam Brindle <brindlejsam@gmail.com>
2022-07-27 22:40:22 -0700

    added ablitity to test if user is cheating, as well as if they get a zilch

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -25,8 +25,9 @@ class GameLogic:
         if len(score_dict) == 3:
             for num in score_dict:
                 if score_dict[num] != 2:
-                    return 0
-            return 1500
+                    score = 0
+                else:
+                    1500
 
         # handles all normal hands
         for num in score_dict:
@@ -90,35 +91,107 @@ def str_to_list_to_tuple(bank_choices):
     return tuple(new_list)
 
 
+def welcome():
+    print("Welcome to Ten Thousand\n(y)es to play or (n)o to decline")
+
+
+def decline_game():
+    print("OK. Maybe another time")
+
+def handle_roll_bank_quit(roll_bank_or_quit, final_score, temp_score, round):
+    if roll_bank_or_quit == 'q':
+        print(f"Thanks for playing. You earned {final_score} points")
+        return
+    elif roll_bank_or_quit == 'b':
+        bank = temp_score
+        final_score += bank
+        temp_score = 0
+        print(f"You banked {bank} points in round {round}")
+        round += 1
+        print(f"Total score is {final_score} points")
+        die_in_play = 6
+        return
+    elif roll_bank_or_quit == 'r':
+        temp_score = 0
+        return
+
+
+def start_round(current_round, die_in_play, user_choice, temp_score):
+    if not user_choice:
+        print(f"Starting round {current_round}")
+
+    print(f"Rolling {die_in_play} dice...")
+    roll_result = get_roll_result(die_in_play)
+    print(f"*** {tuple_to_str(roll_result)}***")
+    if check_for_zilch(roll_result):
+        print("****************************************\n**        Zilch!!! Round over         **\n**************"
+              "**************************")
+        print(f"You banked {temp_score} points in round {current_round}")
+        return
+    print("Enter dice to keep, or (q)uit:")
+    return tuple_to_str(roll_result)
+
+def check_for_zilch(roll_result):
+    if GameLogic.calculate_score(roll_result) == 0:
+        return True
+    return False
+
+
+def get_roll_result(die_in_play):
+    return GameLogic.roll_dice(die_in_play)
+
+
+def check_for_cheater(user_choice, die_roll_data):
+    for char in user_choice:
+        if char not in die_roll_data:
+            print("Hey, its not cool to cheat! Lets restart this round eh?")
+            return True
+
+    return False
+
+
+def handle_zilch(user_input):
+    if user_input == 0:
+        print("Zilch! Your input had a value of 0 and you now get 0 for the round! Better luck next round :)")
+        return True
+    return False
+
+
 def play_game(logic):
 
     final_score = 0
-    round_score = 0
     temp_score = 0
-    bank = 0
-    round = 1
+    re_roll_score = 0
+    current_round = 1
     die_in_play = 6
-    print("Welcome to Ten Thousand\n(y)es to play or (n)o to decline")
+    re_roll = False
+    welcome()
 
     start = input("> ")
 
     if start == 'n':
-        print("OK. Maybe another time")
+        decline_game()
     elif start == 'y':
         while True:
-            print(f"Starting round {round}")
-            print(f"Rolling {die_in_play} dice...")
-            roll_result = GameLogic.roll_dice(die_in_play)
-            print(f"*** {tuple_to_str(roll_result)}***")
-            print("Enter dice to keep, or (q)uit:")
+            die_roll_result = start_round(current_round, die_in_play, re_roll, temp_score)
+            re_roll = False
+            temp_score = re_roll_score
+            re_roll_score = 0
 
             while True:
                 choice = input("> ")
                 if choice == 'q':
                     print(f"Thanks for playing. You earned {final_score} points")
                     return
-                temp_score = GameLogic.calculate_score(str_to_list_to_tuple(choice))
+                if check_for_cheater(choice, die_roll_result):
+                    break
+                temp_score += GameLogic.calculate_score(str_to_list_to_tuple(choice))
+                if handle_zilch(temp_score):
+                    current_round += 1
+                    break
                 die_in_play -= len(choice)
+                if die_in_play == 0:
+                    die_in_play = 6
                 print(f"You have {temp_score} unbanked points and {die_in_play} dice remaining")
                 print("(r)oll again, (b)ank your points or (q)uit:")
                 roll_bank_or_quit = input("> ")
@@ -126,16 +199,16 @@ def play_game(logic):
                     print(f"Thanks for playing. You earned {final_score} points")
                     return
                 elif roll_bank_or_quit == 'b':
-                    bank = temp_score
-                    final_score += bank
+                    final_score += temp_score
+                    print(f"You banked {temp_score} points in round {current_round}")
+                    current_round += 1
                     temp_score = 0
-                    print(f"You banked {bank} points in round {round}")
-                    round += 1
                     print(f"Total score is {final_score} points")
                     die_in_play = 6
                     break
                 elif roll_bank_or_quit == 'r':
-                    temp_score = 0
+                    re_roll = True
+                    re_roll_score = temp_score
                     break
 
     else:
